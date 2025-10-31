371970628106ad9c861ed20d3868e9205f3eb7a6
Noh <nohyam0305@gmail.com>
2025-10-31 18:44:26 +0900

    wip: refactor theme

---
+++ i_am_rich_plus/lib/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final _appBar = AppBarTheme(
+    elevation: 0,
+    centerTitle: true,
+    backgroundColor: Colors.blueGrey[900],
+    foregroundColor: Colors.white,
+  );
+
+  static ThemeData light = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blueGrey,
+      brightness: Brightness.light,
+    ),
+    appBarTheme: _appBar,
+  );
+
+  static ThemeData dark = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blueGrey,
+      brightness: Brightness.dark,
+    ),
+    appBarTheme: _appBar,
+  );
+}
